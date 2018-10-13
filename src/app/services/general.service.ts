fca3acf590932c075dc8bc6c8438475146606ba8
CORPORATE\krb0916 <kevin.blake@daugherty.com>
2018-10-13 09:11:57 -0500

    add headers

--- src/app/services/general.service.ts
+++ src/app/services/general.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
@@ -6,7 +6,13 @@ import { Injectable } from '@angular/core';
 export class GeneralService {
   constructor(private httpClient: HttpClient) {}
 
+ 
   public getText(): Observable<any> {
-    return this.httpClient.get('http://localhost:7777/hello');
+    var httpOptions = {
+      headers: new HttpHeaders({ 
+        'Access-Control-Allow-Origin':'*'
+      })
+    };
+    return this.httpClient.get('http://localhost:7777/hello', httpOptions);
   }
 }
