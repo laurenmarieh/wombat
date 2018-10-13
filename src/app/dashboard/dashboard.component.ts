b96d64c2e9db6c3054766756d562350ee0ccc7cd
Lauren Hilton <lauren.hilton@daugherty.com>
2018-10-12 22:52:05 -0500

    add basic cards to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DashboardCard } from '../models/dashboard-card.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  constructor() { }
+  dashboardCards: DashboardCard[];
 
-  ngOnInit() {
+  constructor() {
+    this.dashboardCards = [
+      {
+        title: 'Test',
+        imageUrl: '../../assets/images/trees.jpg',
+        description: 'test',
+        route: '/dashboard'
+      },
+      {
+        title: 'Test',
+        imageUrl: '../../assets/images/city.jpg',
+        description: 'test',
+        route: '/dashboard'
+      },
+      {
+        title: 'Test',
+        imageUrl: '../../assets/images/lake.jpg',
+        description: 'test',
+        route: '/dashboard'
+      }
+    ];
   }
-
 }
---
+++ src/app/models/dashboard-card.model.ts
@@ -0,0 +1,6 @@
+export class DashboardCard {
+  title: string;
+  imageUrl: string;
+  route: string;
+  description: string;
+}
