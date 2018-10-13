e6f262470be3947a6f927466d4924ba383e31dea
Lauren Hilton <lauren.hilton@daugherty.com>
2018-10-12 21:37:40 -0500

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -14,6 +18,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
+    BsDropdownModule.forRoot(),
+    ButtonsModule.forRoot(),
+    CollapseModule.forRoot(),
     FormsModule,
     AppRoutingModule,
     HttpClientModule
@@ -21,6 +28,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   providers: [],
   bootstrap: [
     AppComponent
+  ],
+  exports: [
+    BsDropdownModule,
+    ButtonsModule,
+    CollapseModule
   ]
 })
 export class AppModule { }
