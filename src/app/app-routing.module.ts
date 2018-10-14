3a4f0f1f33b397e57cf8ef6b0d377b8b65407e71
CORPORATE\krb0916 <kevin.blake@daugherty.com>
2018-10-13 22:38:29 -0500

    fixed thing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ForumComponent } from './forum/forum.component';
 import { ForumDetailComponent } from './forum-detail/forum-detail.component';
-<<<<<<< HEAD
 import { LoginPageComponent } from './login-page/login-page.component';
-=======
 import { ProfileViewComponent } from './profile-view/profile-view.component';
->>>>>>> b90264fba50ee673e2608b286433875cf3389998
-
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -19,11 +15,8 @@ const routes: Routes = [
   { path: 'speak', component: SpeakComponent},
   { path: 'forum', component: ForumComponent },
   { path: 'forum/:id', component: ForumDetailComponent },
-<<<<<<< HEAD
-  { path: 'login', component: LoginPageComponent }
-=======
+  { path: 'login', component: LoginPageComponent },
   { path: 'profile', component: ProfileViewComponent }
->>>>>>> b90264fba50ee673e2608b286433875cf3389998
 ];
 
 @NgModule({
